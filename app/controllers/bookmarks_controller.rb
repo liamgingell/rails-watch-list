a57b01ac3246b7f5526a26fb9c8a7769940c46a1
Liam Gingell <elgee95@gmail.com>
2025-01-11 16:55:06 +0900

    Still some bugs

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,4 +1,6 @@
 class BookmarksController < ApplicationController
+  before_action :set_list, only: [:new, :create, :destroy]
+
   def index
     @bookmarks = Bookmark.all
   end
@@ -13,22 +15,31 @@ class BookmarksController < ApplicationController
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = List.find(params[:bookmark][:list_id])
+    @bookmark.movie = Movie.find_or_create_by(title: params[:bookmark][:movie_title]) do |movie|
+      movie.overview = "Default overview" # You might want to handle this differently
+    end
+
     if @bookmark.save
-      redirect_to bookmark_path(@bookmark)
+      redirect_to list_path(@bookmark.list), notice: 'Bookmark was successfully created.'
     else
       render :new
+    end
   end
 
   def destroy
     @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to bookmarks_path
+    redirect_to list_path(@bookmark.list), notice: 'Bookmark was successfully deleted.'
   end
 
   private
 
-  def bookmark_params
-    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  def set_list
+    @list = List.find(params[:list_id])
   end
-end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :rating, :list_id)
+  end
 end
